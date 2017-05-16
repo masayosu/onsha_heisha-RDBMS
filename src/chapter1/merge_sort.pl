ce0b69176830f45537b26ed02354ae5840e7227d
maezawa.m <maezawa.m@water-cell.jp>
2017-05-16 17:58:50 +0900

    add chapter1

---
+++ src/chapter1/merge_sort.pl
@@ -0,0 +1,60 @@
+#! /usr/bin/env perl
+use strict;
+use warnings;
+
+my @lines;
+
+open (READ, "<", "./merge_sort.txt");
+while(<READ>){
+    chomp;
+    push @lines, $_;
+}
+close READ;
+
+mergeSort(\@lines);
+
+foreach(@lines){ print $_."\n"; } 
+
+sub mergeSort{
+    my $a = shift;
+    my $a_cnt = @$a;
+
+    if ($a_cnt > 1) {
+        my $m = int($a_cnt / 2);
+        my $n = $a_cnt - $m; 
+    
+        my @a1;
+        my @a2;
+        for( my $i = 0; $i < $m; $i++){ push @a1, $a->[$i];}
+        for( my $i = 0; $i < $n; $i++){ push @a2, $a->[$m + $i];}
+
+        mergeSort(\@a1);
+        mergeSort(\@a2);
+
+        merge(\@a1, \@a2, $a);
+    
+    }
+}
+
+sub merge{
+    my $a1 = shift;
+    my $a2 = shift;
+    my $a = shift;
+
+    my $a1_cnt = @$a1;
+    my $a2_cnt = @$a2;
+
+    my $i = 0;
+    my $j = 0;
+
+    while($i < $a1_cnt || $j < $a2_cnt){
+        if ($j >= $a2_cnt || ( $i < $a1_cnt && $a1->[$i] < $a2->[$j]) ){
+            $a->[$i+$j] = $a1->[$i];
+            $i++;
+        }
+        else{
+            $a->[$i+$j] = $a2->[$j];
+            $j++;
+        }
+    }
+}
